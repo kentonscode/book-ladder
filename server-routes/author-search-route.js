187382edb8fe11c20b101259b83d63cfc3753d58
kentonscode <kentonscode@gmail.com>
2015-12-14 15:22:15 -0800

    #1 data returned to both server and client for author searches

--- server-routes/author-search-route.js
+++ server-routes/author-search-route.js
@@ -2,10 +2,11 @@ var express = require('express');
 var authorRoute = express.Router();
 var httpRequest = require('request');
 
-authorRoute.get ('/:author', function(request, response, next) {
+authorRoute.get ('/:author', function(request, res) {
   httpRequest('https://www.googleapis.com/books/v1/volumes?q=+inauthor:' + request.params.author + '&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
     function(errors, response, body) {
       console.log(body);
+      res.send(body);
     });
 });
 
