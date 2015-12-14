e6ce994ed53498e9601664c111bf2a8035e390f6
kentonscode <kentonscode@gmail.com>
2015-12-14 14:46:27 -0800

    #1 search returns JSON data to server. Wokring on showing same in client

--- server-routes/author-search-route.js
+++ server-routes/author-search-route.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var authorRoute = express.Router();
+var httpRequest = require('request');
 
-authorRoute.get ('/:name', function(request, response) {
-  response.send('Hello World');
+authorRoute.get ('/:author', function(request, response, next) {
+  httpRequest('https://www.googleapis.com/books/v1/volumes?q=+inauthor:' + request.params.author + '&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    function(errors, response, body) {
+      console.log(body);
+    });
 });
 
 module.exports = authorRoute;
