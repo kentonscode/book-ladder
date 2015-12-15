e4a1138a8c87bd856a657e2ed8e1eb107a8fcb5d
kentonscode <kentonscode@gmail.com>
2015-12-15 14:33:06 -0800

    #1 setting up http request via angular to show in view

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,4 +1,7 @@
-app.controller('BookSearch',
-  [function() {
-    this.title = 'Testing expression';
-  }]);
+app.controller('authorSearch', ['$http', function ($http) {
+  vm = this;
+  vm.search = function() {
+    $http.get("https://www.googleapis.com/books/v1/volumes/ewMRAAAAIAAJ")
+  .then(function (response) {vm.books = response.data;});
+};
+}]);
