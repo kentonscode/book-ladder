f9888ebad84101f953d01f44314ff646032ab3fa
kentonscode <kentonscode@gmail.com>
2015-12-15 10:42:19 -0800

    #1 title search now returning JSON as well

--- server-routes/title-search-route.js
+++ server-routes/title-search-route.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var titleRoute = express.Router();
+var httpRequest = require('request');
 
-titleRoute.get ('/:title', function(request, response) {
-  response.send('Hello World');
+titleRoute.get ('/:title', function(request, res) {
+  httpRequest('https://www.googleapis.com/books/v1/volumes?q=' + request.params.title + '&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    function(errors, response, body) {
+       console.log(body);
+      res.send(body);
+    });
 });
 
 module.exports = titleRoute;
+
+
