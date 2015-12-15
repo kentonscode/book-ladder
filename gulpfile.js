06d50c8b59dd12393a8d871a1728a1b1ce4f766b
kentonscode <kentonscode@gmail.com>
2015-12-15 09:54:24 -0800

    placed gulp task in seperate files within gulp folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,5 @@
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var mocha = require('gulp-mocha');
+require('require-dir')('./gulp');  
 
-gulp.task('test', function() {
-  return gulp.src('test.js', {read: false})
-  .pipe(mocha({reporter: 'landing'}));
-});
-
-gulp.task('nodemon', function() {
-  nodemon({
-    script: 'app.js',
-  })
-  .on('start', ['test.js']);
-});
 
 gulp.task('default', ['nodemon']);
