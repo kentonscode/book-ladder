0b11ed7b281b7f7f2cc8c404a9daf4107c8d8dc4
kentonscode <kentonscode@gmail.com>
2015-12-16 13:32:31 -0800

    #1 added ng-repeat to iterate through returned data and bind to view

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,6 +2,6 @@ app.controller('authorSearch', ['$http', function ($http) {
   vm = this;
   vm.search = function() {
     $http.get('http://localhost:1337/author-search/' + vm.term )
-  .then(function (response) {vm.books = response.data;});
+  .then(function (response) {vm.books = response.data.items;});
 };
 }]);
