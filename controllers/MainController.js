1ce3cce449eb6d8d8a01f967c43b834964e10a00
kentonscode <kentonscode@gmail.com>
2015-12-23 16:59:24 -0800

    #1 setting up controller for title search request

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -5,3 +5,11 @@ app.controller('authorSearch', ['$http', function ($http) {
   .then(function (response) {vm.books = response.data.items;});
 };
 }]);
+
+app.controller('titleSearch', ['$http', function ($http) {
+  vm = this;
+  vm.search = function() {
+    $http.get('http://localhost:1337/title-search/' + vm.term )
+  .then(function (response) {vm.books = response.data.items;});
+};
+}]);
